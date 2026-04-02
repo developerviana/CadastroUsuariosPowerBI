25df39aa2834de0b76806923fa8785acddf9b769
vianadevelop <flood.com@hotmail.com>
2026-04-02 15:30:50 -0300

    feat: aplica menu lateral po-ui e ajusta titulo da rotina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,29 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-
-import {
-  PoDividerModule,
-  PoMenuItem,
-  PoMenuModule,
-  PoPageModule,
-  PoToolbarModule,
-} from '@po-ui/ng-components';
-import { ExpenseService } from './services/expense.service';
-import { Despesa } from './models/despesa-model';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { PoButtonModule, PoMenuItem, PoMenuModule, PoNotificationService } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    PoToolbarModule,
-    PoMenuModule,
-    PoPageModule,
-    PoDividerModule
-  ],
+  imports: [RouterOutlet, PoMenuModule, PoButtonModule],
   templateUrl: './app.component.html',
-  providers: [ExpenseService],
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private expendService: ExpenseService = inject(ExpenseService);
+  private readonly router = inject(Router);
+  private readonly notification = inject(PoNotificationService);
 
-  public menuItemSelected: string = '';
-  public dados: Despesa[] = [];
-  loading: boolean = true;
-
-  async ngOnInit() {
-    await this.loadData();
-    this.changeMenuSelected(this.menus[0]);
-  }
-
-  private changeMenuSelected(menu: PoMenuItem) {
-    this.menuItemSelected = menu.label;
-  }
-
-  loadData() {
-    return new Promise((resolve, reject) => {
-      this.expendService.getExpends().subscribe({
-        next: data => {
-          this.loading = false;
-          this.dados = data.items;
-          this.menus[0].badge = { value: data.total, color: 'color-09' };
-          resolve(data);
-        },
-        error: err => {
-          this.loading = false;
-          reject(err);
-        }
-      });
-    })
-  }
-
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Listagem', icon: 'po-icon-users', link: '/', action: this.changeMenuSelected.bind(this), shortLabel: 'Listagem' },
-    { label: 'Cadastro', icon: 'po-icon-user-add', link: '/cadastro', action: this.changeMenuSelected.bind(this), shortLabel: 'Cadastro' },
+  public readonly menuItems: PoMenuItem[] = [
+    {
+      label: 'Cadastro de Usuarios Power BI',
+      icon: 'po-icon-users',
+      link: '/'
+    }
   ];
+
+  public exitRoutine(): void {
+    this.notification.information('Saindo da rotina...');
+    this.router.navigateByUrl('/');
+  }
 }
